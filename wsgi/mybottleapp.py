98a2dfaa1d1168d46a1a740b08f919e308bf3515
Jose Maria Castillo <joseccotan10@gmail.com>
2016-05-23 12:30:55 +0200

    subida ficheros final

--- wsgi/mybottleapp.py
+++ wsgi/mybottleapp.py
@@ -1,6 +1,4 @@
 from bottle import route,default_app, get, post, template, request, static_file, response,redirect
-#from requests_oauthlib import OAuth2Session
-#from oauthlib.oauth2 import TokenExpiredError
 import requests
 import json
 import unicodedata
